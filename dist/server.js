087f0ef8615fadf547039bf37dfab658ff605322
sorlov <seekwell@gmail.com>
2019-07-02 17:48:49 +0300

    [+] server

---
+++ dist/server.js
@@ -0,0 +1,21 @@
+var template = "index.html";
+
+const http = require("http");
+
+const hostname = "92.53.91.207";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  ejs.renderFile(template, function(err, result) {
+    if (err) {
+      console.error(err);
+    }
+    res.end(result);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
